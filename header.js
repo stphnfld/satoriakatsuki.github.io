174be0257b548c3c27ca60cdfd9a4d3479c7e2d8
Stephen Flood <stphnfld@gmail.com>
2023-09-01 21:04:31 -0700

    more updates

--- header.js
+++ header.js
@@ -3,8 +3,8 @@ var headerHtml = `
   <header>
     <nav>
       <ul>
-        <li><a href="/"><img src="smiely.png" class="headerlogo"></img></a></li>
-        <li><a href="/about.html">About</a></li>
+        <li><a href="/"><img src="smiley.png" class="headerlogo"></img></a></li>
+        <li><a href="/about.html" class="selected">About</a></li>
         <li><a href="https://www.youtube.com/channel/satoriakatsuki" target="_blank">YouTube</a></li>
         <li><a href="/contact.html">Contact</a></li>
       </ul>
@@ -14,3 +14,13 @@ var headerHtml = `
 
 var body = document.querySelector('body');
 body.insertAdjacentHTML('afterbegin', headerHtml);
+
+// Get the current URL path
+var currentPath = window.location.pathname;
+
+// Add "selected" class to the link that matches the current URL path
+document.querySelectorAll('a').forEach(function(link) {
+    if (link.getAttribute('href') === currentPath) {
+        link.classList.add('selected');
+    }
+});
