f8618e8e6da987b1e0230077f54796980985ad44
Stephen Flood <stphnfld@gmail.com>
2023-09-01 21:21:04 -0700

    header fix

--- header.js
+++ header.js
@@ -7,7 +7,7 @@ var headerHtml = `
         <li><a href="/about.html">About</a></li>
         <li><a href="https://www.youtube.com/channel/satoriakatsuki" target="_blank">YouTube</a></li>
         <li><a href="/contact.html">Contact</a></li>
-        <li><a href="shop.satoriakatsuki.com" class="disabled coming-soon">Merch</a></li>
+        <li><a href="shop.satoriakatsuki.com" class="disabled coming-soon" data-tooltip="Coming Soon!">Merch</a></li>
       </ul>
     </nav>
   </header>
