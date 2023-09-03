45e4f3ba751cd48b7328e770089e0dcbff13f95d
Stephen Flood <stphnfld@gmail.com>
2023-09-02 20:29:53 -0700

    falling squares javascript

---
+++ falling-squares.js
@@ -0,0 +1,41 @@
+const header = document.querySelector('header');
+const colors = ['#99B898', '#FECEAB', '#E84A5F', '#2A363B'];
+
+function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)];
+}
+
+function spawnSquare() {
+    const square = document.createElement('div');
+    square.classList.add('falling-square');
+
+    // Set the square to a random color from the list
+    square.style.backgroundColor = getRandomColor();
+
+    // Position the square at a random horizontal position
+    square.style.left = Math.random() * header.clientWidth + 'px';
+
+    // Set a random rotation angle
+    square.style.transform = 'rotate(' + Math.random() * 360 + 'deg)';
+
+    header.appendChild(square);
+
+    // Animation
+    square.animate([
+        { top: '-10px' }, 
+        { top: header.clientHeight + 'px', offset: 0.99 },
+        { top: header.clientHeight + 'px', opacity: 0 }
+    ], {
+        duration: Math.random() * 2000 + 3000, // Random duration between 3-5 seconds
+        easing: 'linear'
+    }).onfinish = () => {
+        header.removeChild(square);
+    };
+}
+
+function startAnimation() {
+    spawnSquare();
+    setInterval(spawnSquare, 1000); // Adjust this interval for faster or slower square spawning
+}
+
+window.addEventListener('load', startAnimation);
