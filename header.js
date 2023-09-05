5712b4e139dca4f6aaf351e07afa5447c1d00719
Stephen Flood <stphnfld@gmail.com>
2023-09-05 14:37:36 -0700

    major updates

--- header.js
+++ header.js
@@ -3,7 +3,7 @@ var headerHtml = `
 <header>
     <nav>
         <ul id="header-nav">
-            <li><a href="/"><img src="smiley.png" class="headerlogo"></a></li>
+            <li><a href="/"><img src="/images/smiley.png" class="headerlogo"></a></li>
             <li><a href="/about.html">About</a></li>
             <li><a href="https://www.youtube.com/channel/satoriakatsuki" target="_blank">YouTube</a></li>
             <li><a href="/contact.html">Contact</a></li>
