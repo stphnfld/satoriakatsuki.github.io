c75e66e53c9a1962df36d5ea102a5bb8135ed3f1
Stephen Flood <stphnfld@gmail.com>
2023-09-11 16:21:36 -0700

    updates to a lot of places

--- header.js
+++ header.js
@@ -4,9 +4,9 @@ var headerHtml = `
     <nav>
         <ul id="header-nav">
             <li><a href="/"><img src="/images/smiley.png" class="headerlogo"></a></li>
-            <li><a href="/about.html">About</a></li>
+            <li><a href="/about/">About</a></li>
             <li><a href="https://www.youtube.com/channel/satoriakatsuki" target="_blank">YouTube</a></li>
-            <li><a href="/contact.html">Contact</a></li>
+            <li><a href="/contact/">Contact</a></li>
             <li><a href="shop.satoriakatsuki.com" class="disabled coming-soon" data-tooltip="Coming Soon!">Merch</a></li>
         </ul>
     </nav>
